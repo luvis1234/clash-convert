5329f9ef98469d10369ce0fab5e6d46cc52fc63f
luvis1234 <lianhua06@protonmail.com>
2026-07-03 10:40:21 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 # --- 配置区 ---
 SOURCE_URLS = [
    
-    "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/domains/ultimate.txt"  ## 源文件网址之间用,隔开，最后一个不加
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus-onlydomains.txt"  ## 源文件网址之间用,隔开，最后一个不加
 ]
 
 LOCAL_FILES = ["data.txt"]
