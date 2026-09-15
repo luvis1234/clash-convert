c5101214fb75424d10ed46d992a3dfa3d32eeec1
luvis1234 <lianhua06@protonmail.com>
2026-09-15 14:09:45 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -18,7 +18,8 @@ SOURCE_URLS = [
    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate-onlydomains.txt",
    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/urlshortener-onlydomains.txt",
    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/native.winoffice-onlydomains.txt",
-   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.mini-onlydomains.txt"
+   "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.mini-onlydomains.txt",
+   "https://github.com/hagezi/dns-blocklists/blob/main/ips/tif.txt"
 ]
 
 LOCAL_FILES = ["data.txt"]
