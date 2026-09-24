b611c430fe5387b209771e8875f8a8370bed9d30
luvis1234 <lianhua06@protonmail.com>
2026-09-24 09:27:36 +0800

    Update myconvert.py

--- myconvert.py
+++ myconvert.py
@@ -6,8 +6,8 @@ from datetime import datetime, timezone
 # --- 配置区 ---
 # 1. 明确声明为 Clash 混合规则的源 (支持自动识别其中混杂的泛域名和纯域名)
 CLASH_WILDCARD_SOURCES = [
-    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockmihomo.yaml",
-    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/AdvertisingTest/AdvertisingTest_Domain.yaml"
+    #"https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockmihomo.yaml",
+    #"https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/AdvertisingTest/AdvertisingTest_Domain.yaml"
     
     #"https://github.com/luvis1234/tracker/blob/main/antiad.yaml"
     # "https://example.com/clash_myad.yaml",
