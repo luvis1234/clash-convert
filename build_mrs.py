0eb6d2970e58fd56e4180549e3bd06b3178b4a4a
luvis1234 <lianhua06@protonmail.com>
2026-09-25 23:01:37 +0800

    Update build_mrs.py

--- build_mrs.py
+++ build_mrs.py
@@ -8,6 +8,7 @@ from pathlib import Path
 RULE_URLS = [
     # 替换为你真实的规则链接
     "https://raw.githubusercontent.com/luvis1234/clash-convert/refs/heads/main/ruleset.yaml",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/Google/Google.yaml",
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/AdvertisingTest/AdvertisingTest_Domain.yaml",
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/ChinaMaxNoIP/ChinaMaxNoIP_Domain.yaml"
 ]
